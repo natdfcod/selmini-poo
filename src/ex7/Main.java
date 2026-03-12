bc76030b65ff4590d77b567f66eed77e3cfad758
Laboratório FIAP <nick10052007@gmail.com>
2026-03-12 09:42:15 -0300

    final da aula

--- src/ex7/Main.java
+++ src/ex7/Main.java
@@ -6,10 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MonitorEnergia monitor = new MonitorEnergia(10);
+        int dia = 1;
 
-        monitor.registroEnergiaPorDia(2);
-        monitor.registroEnergiaPorDia(2);
-        monitor.registroEnergiaPorDia(4);
+        do {
+            System.out.printf("Digite a valor do dia %d: ", dia);
+            dia = sc.nextInt();
+            monitor.registroEnergiaPorDia(dia);
+        } while (dia != 0);
 
         System.out.printf("%.2f",monitor.calcularMedia());
         System.out.printf("\n%.2f",monitor.calcularMedia(1, 2));
