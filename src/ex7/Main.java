d3df3822234bbc0334c2e86b3d12b6b4e92c35d7
Laboratório FIAP <nick10052007@gmail.com>
2026-03-12 09:20:07 -0300

    meio da aula

---
+++ src/ex7/Main.java
@@ -0,0 +1,21 @@
+package ex7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MonitorEnergia monitor = new MonitorEnergia(10);
+
+        monitor.registroEnergiaPorDia(2);
+        monitor.registroEnergiaPorDia(2);
+        monitor.registroEnergiaPorDia(4);
+
+        System.out.printf("%.2f",monitor.calcularMedia());
+        System.out.printf("\n%.2f",monitor.calcularMedia(1, 2));
+
+
+
+
+    }
+}
